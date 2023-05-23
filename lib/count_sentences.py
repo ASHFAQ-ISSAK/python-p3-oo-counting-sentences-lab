5765703c4c8ad61decd0cbfc0eba2da464ece132
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-23 17:48:07 +0300

    created sentence class with methods

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
-
 class MyString:
-  pass
+    """MyString in count_sentences.py"""
+
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        return False
+
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        return False
+
+    def count_sentences(self):
+        return self.value.count(".") + self.value.count("?") + self.value.count("!")
